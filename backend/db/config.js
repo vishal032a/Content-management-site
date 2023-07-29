e8487500e24c8c216e66df71899b8da659bb05cc
vishal <vk7821188@gmail.com>
2023-07-29 08:33:00 +0530

    final_change

--- backend/db/config.js
+++ backend/db/config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const Connection = async(username,password)=>{
-    const URL = `mongodb+srv://vishal032a:vishal032a@cluster0.fomyegu.mongodb.net/CMS?retryWrites=true&w=majority`
+    const URL = `mongodb+srv://${username}:${password}@cluster0.fomyegu.mongodb.net/CMS?retryWrites=true&w=majority`
 
     try{
         await mongoose.connect(URL);
